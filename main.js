e893b29f561770e6ce57ae8066ddcf2524c8dc8e
Jonathan Valencia <javalencis@gmail.com>
2024-05-24 14:00:57 -0500

    Creación del proyecto y estilos

---
+++ main.js
@@ -0,0 +1,18 @@
+const cursor = document.querySelector('.cursor')
+
+
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + 'px'
+    cursor.style.left = e.pageX + 'px'
+})
+
+window.addEventListener('mousedown', () => {
+    cursor.classList.add('active')
+})
+window.addEventListener('mouseup', () => {
+    cursor.classList.remove('active')
+})
+// window.addEventListener('mouseleave', () => {
+//     cursor.classList.remove('active')
+// })
+
