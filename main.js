16ec95e0ce9127a9bb5f330b87232ab42c33d853
Jonathan Valencia <javalencis@gmail.com>
2024-06-07 14:16:35 -0500

    Spray con pintura

--- main.js
+++ main.js
@@ -13,73 +13,90 @@ let speedMole = 1500
 
 
 
-btStart.addEventListener('click',start)
+btStart.addEventListener('click', start)
 
-function run(){
+function run() {
     const i = Math.floor(Math.random() * holes.length)
     const hole = holes[i]
     let mole = document.createElement('img')
     mole.setAttribute('draggable', false);
     mole.classList.add('mole')
     mole.src = 'assets/smileysad.png'
-    
-    mole.addEventListener('click',()=>handleClickMole(mole,hole))
+
+    mole.addEventListener('click', () => handleClickMole(mole, hole))
 
     hole.appendChild(mole)
     timer = setTimeout(() => {
-        hole.removeChild(mole)
+        if (hole.contains(mole)) {
+            hole.removeChild(mole);
+        }
+
         run()
     }, speedMole)
 }
 
 
-function handleClickMole(mole,hole){
+function handleClickMole(mole, hole) {
     clearTimeout(timer)
+    setTimeout(() => {
+        hole.removeChild(paint)
+
+        mole.src = 'assets/smileyhappy.png'
+
+        
+
+
+    }, 500)
     scoreGame++
     score.innerHTML = scoreGame.toString()
-    mole.src = 'assets/smileyhappy.png'
-    mole.removeEventListener('click', handleClickMole);
-    
+    let paint = document.createElement('img')
+    paint.src = 'assets/paint.png'
+    paint.classList.add('paint')
+    hole.appendChild(paint)
     setTimeout(() => {
-        hole.removeChild(mole)
-        run()
-    }, 500);
-}
-
-function start(){
-    cModal.style.display = 'none'
-    cursor.style.backgroundImage = 'url("assets/hammer.png")'
-    body.style.cursor = 'none'
-
-    timerGame = setInterval(() => {
-        if(timeGame === 0){
-            clearInterval(timerGame)
+        if (hole.contains(mole)) {
+            hole.removeChild(mole);
         }
-        time.innerHTML= timeGame.toString()
-        timeGame--
-        speedMole-=20
-        console.log(speedMole);
-    }, 1000);
+
+        run()
+    }, 800);
+
 }
 
-run()
 
-window.addEventListener('mousemove', e => {
-    cursor.style.top = e.pageY + 'px'
-    cursor.style.left = e.pageX + 'px'
-})
+    function start() {
+        cModal.style.display = 'none'
+        cursor.style.backgroundImage = 'url("assets/hammer.png")'
+        body.style.cursor = 'none'
 
-window.addEventListener('mousedown', () => {
-    cursor.classList.add('active')
-})
-window.addEventListener('touchstart', () => {
-    cursor.classList.add('active')
-})
-window.addEventListener('mouseup', () => {
-    cursor.classList.remove('active')
-})
-window.addEventListener('touchend', () => {
-    cursor.classList.remove('active')
-})
+        timerGame = setInterval(() => {
+            if (timeGame === 0) {
+                clearInterval(timerGame)
+            }
+            time.innerHTML = timeGame.toString()
+            timeGame--
+            speedMole -= 20
+        }, 800);
+    }
+
+    run()
+
+    window.addEventListener('mousemove', e => {
+        cursor.style.top = e.pageY + 'px'
+        cursor.style.left = e.pageX + 'px'
+    })
+
+    window.addEventListener('mousedown', () => {
+        cursor.classList.add('active')
+    })
+    window.addEventListener('touchstart', () => {
+        cursor.classList.add('active')
+    })
+    window.addEventListener('mouseup', () => {
+        cursor.classList.remove('active')
+    })
+    window.addEventListener('touchend', () => {
+        cursor.classList.remove('active')
+    })
 
 
