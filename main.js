019168a161c17895365beb1b86944820311280f4
Jonathan Valencia <javalencis@gmail.com>
2024-06-07 15:35:16 -0500

    Boton de volver a jugar

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ const body = document.querySelector('.body')
 const time = document.querySelector('.time')
 const score = document.querySelector('.score')
 const modalGameOver = document.querySelector('.modal-gameover')
+const btAgain = document.querySelector('.bt-again')
 
-
-const holes = [...document.querySelectorAll('.hole')]
+let holes = [...document.querySelectorAll('.hole')]
+let mole = null
+let hole = null
 let timer = null
-let timeGame = 25
+let timeGame = 5
 let timerGame = null
 let scoreGame = 0
 let speedMole = 1500
@@ -18,16 +20,16 @@ let speedMole = 1500
 
 
 btStart.addEventListener('click', start)
-
+btAgain.addEventListener('click', again)
 function run() {
     const i = Math.floor(Math.random() * holes.length)
-    const hole = holes[i]
-    let mole = document.createElement('img')
+    hole = holes[i]
+    mole = document.createElement('img')
     mole.setAttribute('draggable', false);
     mole.classList.add('mole')
     mole.src = 'assets/smileysad.png'
 
-    mole.addEventListener('click', () => handleClickMole(mole, hole))
+    mole.addEventListener('click', () => handleClickMole())
 
     hole.appendChild(mole)
     timer = setTimeout(() => {
@@ -40,23 +42,22 @@ function run() {
 }
 
 
-function handleClickMole(mole, hole) {
+function handleClickMole() {
     clearTimeout(timer)
-    setTimeout(() => {
-        hole.removeChild(paint)
-
-        mole.src = 'assets/smileyhappy.png'
-
-
-
-
-    }, 500)
+    
     scoreGame++
     score.innerHTML = scoreGame.toString()
     let paint = document.createElement('img')
     paint.src = 'assets/paint.png'
     paint.classList.add('paint')
     hole.appendChild(paint)
+    setTimeout(() => {
+        if(hole.contains(paint)){
+
+            hole.removeChild(paint)
+        }
+        mole.src = 'assets/smileyhappy.png'
+    }, 500)
     setTimeout(() => {
         if (hole.contains(mole)) {
             hole.removeChild(mole);
@@ -67,36 +68,57 @@ function handleClickMole(mole, hole) {
 
 }
 
-function gameOver (){
-    if(timeGame === 0){
+function gameOver() {
+    if (timeGame === 0) {
         cModal.style.display = 'flex'
         modalGameOver.style.display = 'flex'
-        cursor.style.display='none'
+        cursor.style.display = 'none'
         body.style.cursor = 'auto'
 
 
     }
 }
 
+function countTimeGame(){
+    timerGame = setInterval(() => {
+        if (timeGame === 0) {
+            gameOver()
+
+            clearInterval(timerGame)
+        }
+        time.innerHTML = timeGame.toString()
+        timeGame--
+        speedMole -= 20
+    }, 1000);
+}
+function again() {
+    clearTimeout(timer)
+    timer = null
+    timeGame = 25
+    timerGame = null
+    scoreGame = 0
+    speedMole = 1500
+    cModal.style.display = 'none'
+    modalGameOver.style.display = 'none'
+    cursor.style.display = 'block'
+    body.style.cursor = 'none'
+    hole.removeChild(mole)
+    countTimeGame()
+    run()
+
+}
 
 function start() {
     cModal.style.display = 'none'
     modalStart.style.display = 'none'
     cursor.style.backgroundImage = 'url("assets/hammer.png")'
     body.style.cursor = 'none'
+    countTimeGame()
+run()
 
-    timerGame = setInterval(() => {
-        if (timeGame === 0) {
-            clearInterval(timerGame)
-        }
-        time.innerHTML = timeGame.toString()
-        timeGame--
-        gameOver()
-        speedMole -= 20
-    }, 800);
+    
 }
 
-run()
 
 window.addEventListener('mousemove', e => {
     cursor.style.top = e.pageY + 'px'
